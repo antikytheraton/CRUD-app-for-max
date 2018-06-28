087e02aa5a201a889b0eb4c89f96e49fa96c1908
Aaron Arredondo Sanchez <binauralvoice@gmail.com>
2018-06-28 11:36:08 -0500

    [ADD] update function

--- src/controllers/max.controller.js
+++ src/controllers/max.controller.js
@@ -12,28 +12,62 @@ exports.data_create = (req, res) => {
             data: req.body.data
         }
     )
-    data.save(err => err ? next(err) : res.send(`Product created successfully`)
-    )
+    data.save(err => err ? res.sendStatus(400) : res.send(`Product created successfully`))
 }
 
 exports.data_details = (req, res) => {
     Max.findOne(
         {fb_id: req.params.fb_id},
-        (err, data) => err ? next(err) : res.send(data)
+        (err, data) => err ? res.sendStatus(400) : res.send(data)
     )
 }
 
-exports.data_update = (req, res) => {
-    Max.findOneAndUpdate(
-        {fb_id: req.params.fb_id},
-        {$set: { data: req.body }},
-        (err, data) =>  err ? next(err) : res.send('Data updated')
-    )
-}
+// exports.data_update = (req, res) => {
+//     Max.findOneAndUpdate(
+//         {fb_id: req.params.fb_id},
+//         {$set: { data: req.body }},
+//         (err, data) =>  err ? res.sendStatus(400) : res.send('Data updated')
+//     )
+// }
 
 exports.data_delete = (req, res) => {
     Max.findOneAndDelete(
         {fb_id: req.params.fb_id},
-        err => err ? next(err) : res.send(`Deleted successfully!`)
+        err => err ? res.sendStatus(400) : res.status(204).send(`Deleted successfully!`)
     )
 }
+
+exports.data_update = (req, res) => {
+    const dataToUpdate = Max.findOneAndRemove(
+        {fb_id: req.params.fb_id},
+        (err, doc) => {
+            const dict = [obj1(req), obj2(doc)].reduce(
+                (r, o) => {
+                    Object.keys(o).forEach(k => r[k] = o[k])
+                    return r
+                }
+            , {})
+            return err || isEmpty(doc || {}) == true ?
+            res.sendStatus(400) :
+            save_again(res, dict, req.params.fb_id)
+        }
+    )
+    dataToUpdate
+}
+
+const save_again = (res, dict, fb_id) => {
+    const data = new Max(
+        {
+            'fb_id': fb_id,
+            'data': dict
+        }
+    )
+    console.log(data)
+    data.save(err => err ? res.sendStatus(400) : res.send(data))
+}
+
+const obj1 = (req) => req.body || {}
+
+const obj2 = (doc) => isEmpty(doc || {}) ? {} : doc._doc.data
+
+const isEmpty = (obj) => Object.keys(obj).length === 0
--- src/models/max.models.js
+++ src/models/max.models.js
@@ -2,9 +2,11 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
+// const dataSchema = new Schema({ data: Object })
+
 const MaxSchema = new Schema({
     fb_id: {type: String, required: true, max: 100},
-    data: {type: Object, required: true}
+    data: {type: Object, default: {}}
 })
 
 module.exports = mongoose.model('Max', MaxSchema)
